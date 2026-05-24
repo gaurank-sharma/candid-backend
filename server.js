78c9f02ea44fc67ecac2453e3057539edb169aeb
Gaurank <sharmagaurank63@gmail.com>
2026-05-25 00:03:05 +0530

    Fix: catch DB connection error, remove mongodb-memory-server from build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const app = express();
-connectDB();
+connectDB().catch(err => console.error('DB connection error:', err.message));
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
