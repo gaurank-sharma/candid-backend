36f9c40c89143f22ec9019b2df97f8af0d953e99
Gaurank <sharmagaurank63@gmail.com>
2026-05-24 23:44:54 +0530

    Fix Vercel crash: await DB connection per request

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const cors = require('cors');
 
 
 const app = express();
-connectDB();
 app.use(cors());
+app.use(async (_req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (err) {
+    res.status(500).json({ msg: 'Database connection failed' });
+  }
+});
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
